d979eea3fdc3280f071101547ebe5c9c354d1416
ivanxgb <ivanxgb@gmail.com>
2022-10-12 14:56:01 -0500

    Added hook for users Added useEffect for load data. Added method for add a user to firebase

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "../form/Form";
+import { addFirebaseData, getFirebaseData } from "../../firebase/firebaseData";
+import { Table } from "../table/Table";
 
 export function Container() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getFirebaseData().then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (user) => {
+    addFirebaseData(user).then((id) => {
+      setUsers([...users, { id, ...user }]);
+    });
+  };
+
   return (
     <>
       <h1 className="text-center">Diplomado CUC Taller 3 - Ivan Gonzalez</h1>
-      <Form />
-
+      <Form addUser={handleAddUser} />
+      <Table users={users} />
     </>
   );
 }
